6005b49b71963a97a70d45a40848505510d0bff9
David Brakman <david.brakman@gmail.com>
2017-05-02 18:03:53 +0200

    Added initial fields

--- app/src/main/java/ksmori/hu/ait/spades/game/GameModel.java
+++ app/src/main/java/ksmori/hu/ait/spades/game/GameModel.java
@@ -1,4 +1,11 @@
 package ksmori.hu.ait.spades.game;
 
+import java.util.ArrayList;
+
 public class GameModel {
+    private Player[] players; //indexed by ID
+    private Card[] trick; //also indexed by player ID
+    private boolean isSpadesBroken;
+    private ArrayList<Card> deck; //nice methods for shuffling, hopefully
+
 }
--- app/src/main/java/ksmori/hu/ait/spades/game/Player.java
+++ app/src/main/java/ksmori/hu/ait/spades/game/Player.java
@@ -1,4 +1,62 @@
 package ksmori.hu.ait.spades.game;
 
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList<Card> cards;
+    public enum Id{
+        NORTH(0,"North"), EAST(1,"East"), SOUTH(2,"South"), WEST(3,"West");
+        private int value;
+        private String text;
+
+        private Id(int value, String text){
+            this.value = value;
+            this.text = text;
+        }
+        public int getValue() {
+            return value;
+        }
+
+        // How Prof avoids storing Enum in Realm
+        public static Id fromInt(int value) throws IllegalArgumentException {
+            for (Id i : Id.values()) {
+                if (i.value == value) {
+                    return i;
+                }
+            }
+            throw new IllegalArgumentException("Only integers 0-3 correspond to Player IDs");
+        }
+
+        public Id next(){
+            return fromInt((this.value + 1)%4);
+        }
+    }
+    private int id;
+    private int bid;
+    private int score;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBid() {
+        return bid;
+    }
+
+    public void setBid(int bid) {
+        this.bid = bid;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
 }
