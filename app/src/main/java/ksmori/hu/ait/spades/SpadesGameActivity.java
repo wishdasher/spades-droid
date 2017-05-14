bc6c6edc72353529163fb5eafe19e6c45765986a
David Brakman <david.brakman@gmail.com>
2017-05-14 20:57:11 +0200

    Wrote layout for 1 of 4 plays on the gametable

--- app/src/main/java/ksmori/hu/ait/spades/SpadesGameActivity.java
+++ app/src/main/java/ksmori/hu/ait/spades/SpadesGameActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import ksmori.hu.ait.spades.view.GameTableFragment;
 
@@ -31,5 +33,20 @@ public class SpadesGameActivity extends AppCompatActivity {
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.fl_game_table_container, mGameTableFragment,GameTableFragment.TAG);
         ft.commit();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.ll_player_box_top);
+        ImageView imageView = (ImageView) findViewById(R.id.iv_player_card_top);
+        LinearLayout textLayout = (LinearLayout) findViewById(R.id.ll_info_box_top);
+
+        if(textLayout.getWidth()+imageView.getWidth() != 0) {
+            layout.getLayoutParams().width = textLayout.getWidth() + imageView.getWidth()
+                    + (int)getResources().getDimension(R.dimen.spacing_card_info);
+            layout.requestLayout();
+        } else {
+            Log.d("SpadesGameActivity","Failed to resize layout: could not retrieve view dimensions");
+        }
     }
 }
--- app/src/main/java/ksmori/hu/ait/spades/view/GameTableFragment.java
+++ app/src/main/java/ksmori/hu/ait/spades/view/GameTableFragment.java
@@ -3,21 +3,27 @@ package ksmori.hu.ait.spades.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ksmori.hu.ait.spades.R;
 
 public class GameTableFragment extends Fragment {
 
     public static final String TAG = "GameTableFragment";
+    private View rootView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_game_table, container, false);
+        rootView = inflater.inflate(R.layout.fragment_game_table, container, false);
 
         return rootView;
     }
+
 }
