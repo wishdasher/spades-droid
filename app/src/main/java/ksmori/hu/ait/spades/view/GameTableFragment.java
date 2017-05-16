91b4483b4d7a53df5de4e59d0e3771fbc0d36839
David Brakman <david.brakman@gmail.com>
2017-05-16 15:57:56 +0200

    Finished first draft of game activity layout

--- app/src/main/java/ksmori/hu/ait/spades/view/GameTableFragment.java
+++ app/src/main/java/ksmori/hu/ait/spades/view/GameTableFragment.java
@@ -1,5 +1,6 @@
 package ksmori.hu.ait.spades.view;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -22,8 +23,41 @@ public class GameTableFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_game_table, container, false);
-
         return rootView;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // called when view becomes visible and has its final size
+        rootView.post(new Runnable() {
+            @Override
+            public void run() {
+                final String[] positions = {"top","left","bottom","right"};
+                for(String str : positions) {
+                    LinearLayout layout = (LinearLayout) getView().findViewById(getResources().getIdentifier(
+                            "player_box_" + str, "id", "ksmori.hu.ait.spades")
+                    );
+                    ImageView imageView = (ImageView) layout.findViewById(getResources().getIdentifier(
+                            "iv_player_card_" + str, "id", "ksmori.hu.ait.spades")
+                    );
+                    LinearLayout textLayout = (LinearLayout) layout.findViewById(getResources().getIdentifier(
+                            "ll_info_box_" + str, "id", "ksmori.hu.ait.spades")
+                    );
+
+                    if (textLayout.getWidth() + imageView.getWidth() != 0) {
+                        layout.getLayoutParams().width = textLayout.getWidth() + imageView.getWidth()
+                                + (int) getResources().getDimension(R.dimen.spacing_card_info);
+                        layout.requestLayout();
+                    } else {
+                        Log.d("SpadesGameActivity", "Failed to resize layout: could not retrieve view dimensions");
+                    }
+                }
+            }
+        });
+    }
+
+
 
 }
