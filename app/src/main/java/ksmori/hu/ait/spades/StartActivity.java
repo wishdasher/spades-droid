eb7eebd965a572b3b83f6a88ed902c447d1e3519
Keiko Mori <ksmori@mit.edu>
2017-05-20 23:08:01 -0400

    Add firebase authentication and a login activity

--- app/src/main/java/ksmori/hu/ait/spades/StartActivity.java
+++ app/src/main/java/ksmori/hu/ait/spades/StartActivity.java
@@ -5,10 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
 
-    private static final String TAG = "StartActivity";
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
